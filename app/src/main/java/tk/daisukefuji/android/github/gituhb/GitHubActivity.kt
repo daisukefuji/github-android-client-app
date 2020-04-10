0a1111f66636edc24b4682cd43624c46f1277d8f
Daisuke FUJI <daisukefuji@users.noreply.github.com>
2020-04-10 10:18:57 +0900

    Create new project

---
+++ app/src/main/java/tk/daisukefuji/android/github/gituhb/GitHubActivity.kt
@@ -0,0 +1,12 @@
+package tk.daisukefuji.android.github.gituhb
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import tk.daisukefuji.android.github.R
+
+class GitHubActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.github_activity)
+    }
+}
