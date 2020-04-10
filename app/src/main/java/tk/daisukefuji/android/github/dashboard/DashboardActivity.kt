82300ecb4467f9ffde9423d0a51d768253ffb8db
Daisuke FUJI <daisukefuji@users.noreply.github.com>
2020-04-10 10:18:57 +0900

    Create new project

---
+++ app/src/main/java/tk/daisukefuji/android/github/dashboard/DashboardActivity.kt
@@ -0,0 +1,12 @@
+package tk.daisukefuji.android.github.dashboard
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import tk.daisukefuji.android.github.R
+
+class DashboardActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.dashboard_activity)
+    }
+}
